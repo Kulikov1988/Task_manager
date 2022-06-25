4492acc80ebacc1c8efd8e9fee9c2fb86bb0f36b
Kulikov1988 <kulikov.tolik@gmail.com>
2022-06-25 13:28:45 +0300

    pesist add

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './store/index';
+import store, {persistor} from './store/index';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-     <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
    ,
@@ -18,7 +21,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,19 +2,42 @@ import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import authReducer from '../slices/authReducer'
 import taskReducer from '../slices/tasksReducer'
+import {
+   persistStore,
+   persistReducer,
+   FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+
 
 const reducer = combineReducers({
   authReducer,
   taskReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false
+    serializableCheck: { 
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    }
   })
 })
 
 export type AppState = ReturnType<typeof reducer>;
 
+export const persistor = persistStore(store);
 export default store;
